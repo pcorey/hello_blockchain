045597684e5866020e3e60bf8963de3ac37c4a7e
Pete Corey <petecorey@gmail.com>
2017-08-31 13:54:07 -0400

    Cleanup

--- lib/hello_blockchain/blockchain/blockchain.ex
+++ lib/hello_blockchain/blockchain/blockchain.ex
@@ -1,51 +1,35 @@
 defmodule HelloBlockchain.Blockchain do
-  @moduledoc """
-  The Blockchain context.
-  """
-
   import Ecto.Query, warn: false
-  alias HelloBlockchain.Repo
 
+  alias HelloBlockchain.Repo
   alias HelloBlockchain.Blockchain.Block
 
   defp bitcoind(command) do
     url = "http://bitcoinrpc:dae10a91212ab0423401b0f079e043ed@localhost:8332"
+    body = %{jsonrpc: "1.0", params: []}
+    |> Map.merge(command)
+    |> Poison.encode!
     headers = [{"Content-Type", "application/json"}]
-    with {:ok, response} <- HTTPoison.post(url, Poison.encode!(command), headers),
+    with {:ok, response} <- HTTPoison.post(url, body, headers),
          {:ok, body} <- Poison.decode(response.body),
          %{"error" => nil, "result" => result} <- body do
       {:ok, result}
     else
       %{"error" => error} -> {:error, error}
-      _ -> {:error, :rpc_failure}
+      _ -> {:error, :jsonrpc_failure}
     end
   end
 
   def getinfo do
-    bitcoind(%{
-      jsonrpc: "1.0",
-      id: "curltext",
-      method: "getinfo",
-      params: []
-    })
+    bitcoind(%{method: "getinfo"})
   end
 
   def getblockhash(index) do
-    bitcoind(%{
-      jsonrpc: "1.0",
-      id: "curltext",
-      method: "getblockhash",
-      params: [index]
-    })
+    bitcoind(%{method: "getblockhash", params: [index]})
   end
 
   def getblock(hash) do
-    bitcoind(%{
-      jsonrpc: "1.0",
-      id: "curltext",
-      method: "getblock",
-      params: [hash]
-    })
+    bitcoind(%{method: "getblock", params: [hash]})
   end
 
 end
